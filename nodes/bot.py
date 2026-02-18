c277fc3056c7b17342bf80cac63b2cb905f7d2ef
ankitsharma38 <ankitsharma7805@gmail.com>
2026-02-18 12:19:53 +0530

    added open ai configuration

--- nodes/bot.py
+++ nodes/bot.py
@@ -2,6 +2,10 @@ from langchain_google_genai import ChatGoogleGenerativeAI
 
 llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", streaming=True)
 
+# from langchain_openai import ChatOpenAI
+
+# llm = ChatOpenAI(model="gpt-4o-mini", streaming=True)
+
 
 def chatbot(state):
     response = llm.invoke(state["messages"])
