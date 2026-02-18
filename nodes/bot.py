309bcd73b104b39641a8a7fff93bf4bfc80016cd
ankitsharma38 <ankitsharma7805@gmail.com>
2026-02-18 13:49:47 +0530

    gemini

--- nodes/bot.py
+++ nodes/bot.py
@@ -1,11 +1,11 @@
 from dotenv import load_dotenv
 load_dotenv()
 
-# from langchain_google_genai import ChatGoogleGenerativeAI
-# llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", streaming=True)
+from langchain_google_genai import ChatGoogleGenerativeAI
+llm = ChatGoogleGenerativeAI(model="gemini-2.5-flash", streaming=True)
 
-from langchain_openai import ChatOpenAI
-llm = ChatOpenAI(model="gpt-4o-mini", streaming=True)
+# from langchain_openai import ChatOpenAI
+# llm = ChatOpenAI(model="gpt-4o-mini", streaming=True)
 
 def chatbot(state):
     response = llm.invoke(state["messages"])
